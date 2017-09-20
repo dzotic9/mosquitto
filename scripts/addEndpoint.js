52348b095f50de06522a99e958bb2c0fbfaf97c5
dzotic9 <dzotic9@gmail.com>
2017-09-20 16:02:55 +0300

    Update addEndpoint.js

--- scripts/addEndpoint.js
+++ scripts/addEndpoint.js
@@ -52,4 +52,4 @@ var jps = {
     }
 };
 
-return jelastic.development.scripting.Eval(APPID, session, "InstallApp", jps);
+return jelastic.development.scripting.Eval(APPID, session, "InstallApp", toJSON(jps));
