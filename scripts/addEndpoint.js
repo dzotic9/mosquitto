7770a9b2e998addc4ac0cf89c723a9231240d760
dzotic9 <dzotic9@gmail.com>
2017-09-20 14:40:23 +0300

    Update addEndpoint.js

--- scripts/addEndpoint.js
+++ scripts/addEndpoint.js
@@ -1,8 +1,5 @@
 //@req(nodeGroup, name, port)
 
-import com.hivext.api.environment.Environment;
-import com.hivext.api.development.Scripting;
-
 var APPID = getParam("TARGET_APPID"),
     SESSION = getParam("session"),
     PROTOCOL = getParam("protocol", "TCP"),
@@ -13,26 +10,30 @@ var APPID = getParam("TARGET_APPID"),
     oResp,
     i;
 
+/*
 oEnvService = hivext.local.exp.wrapRequest(new Environment(APPID, SESSION));
+ 
 oScripting =  hivext.local.exp.wrapRequest(new Scripting({
     serverUrl : "http://" + window.location.host.replace("app", "appstore") + "/",
     session : SESSION
 }));
 
+*/
 
-oEnvInfo = oEnvService.getEnvInfo();
+
+oEnvInfo = jelastic.env.constrol.GetEnvInfo();
 
 if (!oEnvInfo.isOK()) {
     return oEnvInfo;
 }
 
-oEnvInfo = toNative(oEnvInfo);
+//oEnvInfo = toNative(oEnvInfo);
 
 nNodesCount = oEnvInfo.nodes.length;
 
 for (i = 0; i < nNodesCount; i += 1) {
     if (oEnvInfo.nodes[i].nodeGroup == nodeGroup) {
-        oResp = oEnvService.addEndpoint({
+        oResp = jelastic.env.constrol..AddEndpoint({
             name: name,
             nodeid: oEnvInfo.nodes[i].id,
             privatePort: port,
@@ -46,7 +47,7 @@ for (i = 0; i < nNodesCount; i += 1) {
     }
 }
 
-return oScripting.eval({
+return jelastic.development.scripting.Eval({
     script : "InstallApp",
     targetAppid : APPID,
     manifest : toJSON({
