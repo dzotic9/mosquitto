81463f83700580affdb256255d3d96187a2f0d83
dzotic9 <dzotic9@gmail.com>
2017-09-20 16:23:09 +0300

    Update addEndpoint.js

--- scripts/addEndpoint.js
+++ scripts/addEndpoint.js
@@ -2,7 +2,7 @@
 
 import com.hivext.api.environment.Environment;
 import com.hivext.api.development.Scripting;
-import com.hivext.api.billing.GroupQuota;
+import com.hivext.api.Billing.GroupQuota;
 
 var APPID = getParam("TARGET_APPID"),
     SESSION = getParam("session"),
