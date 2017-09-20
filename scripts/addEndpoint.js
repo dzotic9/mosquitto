c55411839ce0ec02f4d864a9f3fa66630cb7b3fe
dzotic9 <dzotic9@gmail.com>
2017-09-20 16:20:00 +0300

    Update addEndpoint.js

--- scripts/addEndpoint.js
+++ scripts/addEndpoint.js
@@ -1,55 +1,67 @@
 //@req(nodeGroup, name, port)
 
+import com.hivext.api.environment.Environment;
+import com.hivext.api.development.Scripting;
+import com.hivext.api.billing.GroupQouta;
+
 var APPID = getParam("TARGET_APPID"),
-    //SESSION = getParam("session"),
+    SESSION = getParam("session"),
     PROTOCOL = getParam("protocol", "TCP"),
     oEnvService,
     oEnvInfo,
     nNodesCount,
     oScripting,
+    bEndPointsEnabled,
     oResp,
     i;
 
-/*
 oEnvService = hivext.local.exp.wrapRequest(new Environment(APPID, SESSION));
- 
+oGroupQoutaService = hivext.local.exp.wrapRequest(new GroupQouta(APPID, SESSION));
 oScripting =  hivext.local.exp.wrapRequest(new Scripting({
     serverUrl : "http://" + window.location.host.replace("app", "appstore") + "/",
     session : SESSION
 }));
 
-*/
+return oGroupQoutaService.GetQuotas();
 
-oEnvInfo = jelastic.env.control.GetEnvInfo(APPID, session);
 
-if (!oEnvInfo || oEnvInfo.result != 0) {
+oEnvInfo = oEnvService.getEnvInfo();
+
+if (!oEnvInfo.isOK()) {
     return oEnvInfo;
 }
 
-//oEnvInfo = toNative(oEnvInfo);
+oEnvInfo = toNative(oEnvInfo);
 
 nNodesCount = oEnvInfo.nodes.length;
 
 for (i = 0; i < nNodesCount; i += 1) {
     if (oEnvInfo.nodes[i].nodeGroup == nodeGroup) {
-        oResp = jelastic.env.control.AddEndpoint(APPID, session, oEnvInfo.nodes[i].id, port, PROTOCOL, name);
+        oResp = oEnvService.addEndpoint({
+            name: name,
+            nodeid: oEnvInfo.nodes[i].id,
+            privatePort: port,
+            protocol: PROTOCOL
+        });
 
-        if (!oResp || oResp.result != 0) {
+        if (!oResp.isOK()) {
             return oResp;
         }
-        //oResp = toNative(oResp);
+        oResp = toNative(oResp);
     }
 }
 
-var jps = {
-    "jpsType" : "update",
-    "application" : {
-        "id": "Mosquitto",
-        "name": "Mosquitto",
-        "success": {
-            "email": "To access your Mosquitto MQTT server, refer to the **${env.domain}** domain name through either *" + oResp.object.publicPort + "* port (for external access from wherever in the Internet) or *1883* port (for connecting within internal Plaform network)"
+return oScripting.eval({
+    script : "InstallApp",
+    targetAppid : APPID,
+    manifest : toJSON({
+        "jpsType" : "update",
+        "application" : {
+            "id": "Mosquitto",
+            "name": "Mosquitto",
+            "success": {
+                "email": "To access your Mosquitto MQTT server, refer to the **${env.domain}** domain name through either *" + oResp.object.publicPort + "* port (for external access from wherever in the Internet) or *1883* port (for connecting within internal Plaform network)"
+            }
         }
-    }
-};
-
-return jelastic.development.scripting.Eval(appid, session, "Install", {manifest : jps, targetAppid: APPID});
+    })
+});
