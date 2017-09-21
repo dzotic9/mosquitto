20e0e86640ab393ff83d3e3dd03deb767dc63ea2
dzotic9 <dzotic9@gmail.com>
2017-09-21 15:28:13 +0300

    Update addEndpoint.js

--- scripts/addEndpoint.js
+++ scripts/addEndpoint.js
@@ -39,7 +39,7 @@ if (bEndPointsEnabled) {
 
     sSuccessText = "To access your Mosquitto MQTT server, refer to the <b>${env.domain}</b> domain name through either<ul><li> <i>" + oResp.object.publicPort + "</i> port (for external access from wherever in the Internet)</li><li> or <i>1883</i> port (for connecting within Plaform internal network, i.e. from another Jelastic container).</li></ul>";
 } else {
-    sSuccessText = "To access your Mosquitto MQTT server, refer to the <b>tcp://${env.domain}: 1883</b> (for connecting within Plaform internal network, i.e. from another Jelastic container).<br><br>Jelastic Endpoints are limited by your quotas. So external access from wherever in the Internet is denied. Please contact to support or upgrade account to increase this possibility. <br><br>After that add Endpoints to your environment on your compute node. More details how to add endpoints are <a href=\'https://docs.jelastic.com/endpoints\'>here.</a>";
+    sSuccessText = "To access your Mosquitto MQTT server, refer to the <b>tcp://${env.domain}: 1883</b> (for connecting within Plaform internal network, i.e. from another Jelastic container).<br><br>For external access from outside the Platform, the Endpoints functionality should be enabled for your account - please, convert to billing or contact support to get this possibility. Then refer to the same-named section within your environment settings and add a new endpoint (with Private Port <i>1883</i> and TCP protocol use) to its compute node.";
 }
 
 return {
